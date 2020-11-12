92a60f4ccdd17a5964fefebaba7e33d10ce229eb
Fernando Doménech <burbujo.fd@gmail.com>
2020-11-12 14:04:33 +0100

    agregado el cementerio al spritesheet y a los mapas. Añadidos nuevos perfiles. Solucionado problema con recoger pedido que provocaba que se mantuviese la velocidad. Se le ha añadido un sonido al coger el pedido

--- src/classes/estados jugador/estadoRecogerPedido.js
+++ src/classes/estados jugador/estadoRecogerPedido.js
@@ -1,6 +1,12 @@
 class estadoRecogerPedido extends State{
 	enter(delta, scene, jugador){
 		RecogerPedido(scene, arrayPedidosPorRecoger[0]);
+		
+		jugador.body.velocity.x = 0;
+		jugador.anims.play("idle", true);
+		if(!jugador.sGuarTarjeta.isPlaying){
+			jugador.sGuarTarjeta.play();
+		}
 	}
 	
 	execute(delta, scene, jugador){
