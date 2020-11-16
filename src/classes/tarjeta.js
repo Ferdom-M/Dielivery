dddc6750039f593e5ceab2f36ca852907a04bbdb
Fernando Doménech <burbujo.fd@gmail.com>
2020-11-16 19:37:21 +0100

    Añadidas las traducciones de las causas de muerte. Configurado el texto final para las tarjetas De parte de Elena: Terminados los degradados

--- src/classes/tarjeta.js
+++ src/classes/tarjeta.js
@@ -1,6 +1,40 @@
+var configTexto = {
+			fontFamily: 'Sylfaen',
+			fontSize: '18px',
+			color: '#fff',
+			stroke: '#000000',
+			strokeThickness: 2,
+			align: 'left',  // 'left'|'center'|'right'|'justify'
+			maxLines: 0,
+			lineSpacing: 0,
+			fixedWidth: 198,
+			fixedHeight: 92 ,
+			rtl: false,
+			testString: '|MÉqgy',
+			wordWrap: {
+				width: 198,
+				callback: null,
+				callbackScope: null,
+				useAdvancedWrap: false
+			},
+			metrics: false
+			// metrics: {
+			//     ascent: 0,
+			//     descent: 0,
+			//     fontSize: 0
+			// }
+		};
+
 class Tarjeta extends Phaser.GameObjects.Container{
 	constructor(scene, x, y, pedido) {
 		var imagenes = new Array();
+		
+		var tituloObjetos;
+		if(idioma.idioma.includes("es")){
+			tituloObjetos = "Ofrendas:"
+		}else{
+			tituloObjetos = "Offerings:";
+		}
 		
 		if(pedido.destinatario){
 			imagenes.push(scene.add.image(0, 0, "cielo" + pedido.numObjetos).setInteractive());
@@ -15,16 +49,10 @@ class Tarjeta extends Phaser.GameObjects.Container{
 		this.imagenes.push(scene.add.image(0, 0, 'persona' + pedido.persona));
 		*/
 		
-		imagenes.push(scene.add.text(-10, -100, arrayNombres[pedido.persona]));
-		imagenes.push(scene.add.text(-10, -50, arrayCausaMuerte[pedido.persona]));
+		imagenes.push(scene.add.text(-10, -104, arrayNombres[pedido.persona], configTexto).setFontStyle('bold'));
+		imagenes.push(scene.add.text(-10, -60, arrayCausaMuerte[pedido.persona], configTexto));
+		imagenes.push(scene.add.text(-150, 30, tituloObjetos, configTexto).setFontStyle('bold').setFontSize('14px'));
 		
-		var tituloObjetos;
-		if(idioma.idioma.includes("es")){
-			tituloObjetos = "Ofrendas:"
-		}else{
-			tituloObjetos = "Offerings:";
-		}
-		imagenes.push(scene.add.text(-147, 39, tituloObjetos));
 		imagenes.push(scene.add.image(-95, -50, "perfil" + (pedido.persona)).setInteractive());
         super(scene, x, y, imagenes);
         
